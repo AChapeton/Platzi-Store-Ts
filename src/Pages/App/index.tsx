ed6b106b0f62cb032c804bfec560d27791eb8b0a
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-05-19 15:53:15 -0600

    Basic pages main files

---
+++ src/Pages/App/index.tsx
@@ -0,0 +1,16 @@
+import { Home } from "../Home"
+import { MyAccount } from "../MyAccount"
+import { MyOrder } from "../MyOrder"
+import { MyOrders } from "../MyOrders"
+import { NotFound } from "../NotFound"
+function App() {
+
+  return (
+    <div className='bg-red-500'>
+      <Home/>
+      <MyAccount/>
+    </div>
+  )
+}
+
+export default App
