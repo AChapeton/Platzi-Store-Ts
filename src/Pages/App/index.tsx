b623be247b5cfcd831acb384abd9aeef81200c77
Andres Chapeton <andres.chapeton0206@gmail.com>
2023-05-22 19:38:15 -0600

    Router structure

--- src/Pages/App/index.tsx
+++ src/Pages/App/index.tsx
@@ -1,15 +1,23 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
 import { Home } from "../Home"
 import { MyAccount } from "../MyAccount"
 import { MyOrder } from "../MyOrder"
 import { MyOrders } from "../MyOrders"
+import {SignIn} from '../SignIn'
 import { NotFound } from "../NotFound"
-function App() {
 
+function App() {
   return (
-    <div className='bg-red-500'>
-      <Home/>
-      <MyAccount/>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home/>}/>
+        <Route path="/my-account" element={<MyAccount/>}/>
+        <Route path="/my-order" element={<MyOrder/>}/>
+        <Route path="/my-orders" element={<MyOrders/>}/>
+        <Route path="/sign-in" element={<SignIn/>}/>
+        <Route path="/not-found" element={<NotFound/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
